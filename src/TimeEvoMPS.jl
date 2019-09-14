837b4f1de62100ca9f33627982e453127ee81ede
orialb <orialb1@gmail.com>
2019-09-14 16:55:46 +0200

    first commit

---
+++ src/TimeEvoMPS.jl
@@ -0,0 +1,8 @@
+module TimeEvoMPS
+
+#TODO : implement exp function for ITensor
+#TODO : can we use AutoMPO object in order to generate bond Hamiltonian? This will be very useful
+#TODO : BondGates object which will store the list of gates
+#TODO : implement tDMRG algorithm (apply gates by sweeping)
+
+end # module

3fc7392a93b31b3c9cbcbbd672ff1ce30bf607f7
orialb <orialb1@gmail.com>
2019-09-24 20:03:21 +0200

    remove exp since it was merged to ITensors

--- src/itensor.jl
+++ src/itensor.jl
@@ -28,23 +28,3 @@ function isrightortho(M,i)
 end
 
 
-function storage_exp(As::Dense{T}, Lis,Ris) where {T}
-    expAdata = LinearAlgebra.exp( reshape(data(As),dim(Lis),dim(Ris)) )
-    return Dense{T}(vec(expAdata))
-end
-
-"""
-    exp(A::ITensor, Lis::IndexSet)
-compute the exponent of the tensor `T` by treating as a matrix ``T_{lr}`` with
-the left index `l` running over all indices in `Lis` and `r` running over all
-indices not in `Lis`. Must have `dim(Lis) == dim(inds(A))/dim(Lis)` for the exponentiation to
-be defined.
-"""
-function exp(A::ITensor, Lis::IndexSet)
-    (dim(Lis) == dim(inds(A))/dim(Lis)) || throw(DimensionMismatch("dimension of the left index set `Lis` must be
-                                                                     equal to `dim(inds(A))/dim(Lis)`"))
-    A, Lis, Ris = _permute_for_factorize(A,Lis)
-    expAs = storage_exp(store(A), Lis,Ris)
-    return ITensor(inds(A),expAs)
-end
-

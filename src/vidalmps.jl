f8b5f6bad4f1f2534637331657f36474d9ea5fc7
orialb <orialb1@gmail.com>
2019-09-27 09:16:30 +0200

    fix bugs

--- src/vidalmps.jl
+++ src/vidalmps.jl
@@ -14,13 +14,14 @@ function VidalMPS(psi::MPS)
     S_ = Vector{ITensor}()
     for b in reverse(1:length(psi)-1)
         orthogonalize!(psi,b)
+        li = linkindex(psi,b)
         if dim(linkindex(psi,b)) > 1
             _,S,_ = svd(psi[b]*psi[b+1],inds(psi[b]) )
             #TODO : using storage interface here, should be a way
             # to do that in a more robust way (maybe Vector(S) for DiagITensor)
-            push!(S_,diagITensor(data(store(S)), linkindex(psi,b)))
+            push!(S_,diagITensor(data(store(S)), IndexSet(li',li)))
         else
-            push!(S_,diagITensor([1.], linkindex(psi,b)))
+            push!(S_,diagITensor([1.], IndexSet(li',li)))
         end
     end
     # orthogonalize!(psi,1)
@@ -35,18 +36,28 @@ ITensors.orthogonalize!(psi::VidalMPS,b) = throw("VidalMPS does not support shif
 Smat(psi::VidalMPS,b) = psi.S_[b]
 setSmat!(psi::VidalMPS,b,S::ITensor) = (psi.S_[b] = S)
 
+function debugortho(M,i)
+    i==1 && return 0
+    R = M[i]*prime(dag(M[i]),i==length(M) ? "Link" : commonindex(M[i],M[i-1]))
+    r = linkindex(M,i-1)
+    return norm(R-delta(r,r'))
+end
+
+
 function apply_gate!(psi::VidalMPS,G::BondGate ; kwargs...)
     b = bond(G)
     wf_ = noprime( G*(psi[b]*psi[b+1]) )
-    wf = b>1 ? Smat(psi,b-1)*wf_ : wf_
-    U,S,V = svd(wf, inds(psi[b]); kwargs...)
+    wf = b>1 ? noprime(Smat(psi,b-1)*wf_ ) : wf_
+    U,S,V,u,v= svd(wf, inds(psi[b]); kwargs...)
     linktag = tags(linkindex(psi,b))
-    replacetags!(V,tags(findinds(V,"Link,v")[1]) , linktag)
-    #FIX: this is not working currently, need to renormalize
-    psi[b] = wf_*V
+    replacetags!(V,tags(v) , linktag)
+    replacetags!(U,tags(u) , linktag)
+
+    normS = sqrt(scalar(S*S))
+
+    psi[b] = wf_*dag(V) / normS
     psi[b+1] = V
-    @assert isrightortho(psi,b)
-    @assert isrightortho(psi,b+1)
-    setSmat!(psi,b,ITensor(data(store(S)), linkindex(psi,b)))
+    li = linkindex(psi,b)
+    setSmat!(psi,b,diagITensor(data(store(S)) ./ normS, IndexSet(li',li)))
     return nothing
 end

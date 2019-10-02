7880b9d5ee7bca1b2145a78e33cccf555ddb2f57
orialb <orialb1@gmail.com>
2019-10-02 19:05:54 +0200

    add TEBD4; docstrings for TEBD algs; test TEBD algs

--- src/tebd.jl
+++ src/tebd.jl
@@ -1,12 +1,56 @@
 export apply_gate!,
-       tebd!, TEBD2, TEBD1
+    tebd!, TEBD2,
+    TEBD2Sweep, TEBD4
 
 abstract type TEBDalg end
 
-struct TEBD1 <: TEBDalg
+"""
+    TEBD2 <: TEBDalg
+TEBD using 2nd order Trotter decomposition of the time evolution
+operator. Namely, if the Hamiltonian is given by ``\\sum_i h_{i,i+1}`` with
+``h_{i,i+1}`` having support only on the sites ``i,i+1``, define
+``H_{e} = \\sum_{i \\el \\mathrm{even}}, H_{o} = \\sum_{i \\el \\mathrm{odd}}``.
+The time-evolution operator is then approximated as:
+
+``U(\\Delta t) = \\exp(-i \\Delta t/2 H_{o}) \\exp(-i \\Delta t H_{e}) \\exp(-i \\Delta t/2 H_{o})``
+
+In practice the ``\\Delta t/2 H_{o}`` terms from consecutive time steps are grouped together as long as
+no measurement is performed. Hence the overhead with respect to 1st order Trotter decomposition is
+minimal.
+
+The Trotter error per time step is ``O(\\Delta t^2)``.
+"""
+struct TEBD2 <: TEBDalg
 end
 
-struct TEBD2 <: TEBDalg
+"""
+    TEBD2Sweep <: TEBDalg
+TEBD using a 2nd Trotter decomposition similar to [`TEBD2`](@ref), but
+instead of an even-odd decomposition of the Hamiltonian the time evolution
+operator is approximated as:
+
+``U(\\Delta t) = e^{-i \\Delta t /2 h_{1,2} } e^{-i \\Delta t /2 h_{2,3}} ... e^{-i \\Delta t /2 h_{2,3}} e^{-i \\Delta t /2 h_{1,2} }``
+
+That is the exponentials of the bond Hamiltonians are applied to each bond sweeping from left to right and then applied a second
+time sweeping from right to left.
+
+The Trotter error per time step is ``O(\\Delta t^2)``
+"""
+struct TEBD2Sweep <: TEBDalg
+end
+
+
+"""
+    TEBD4 <: TEBDalg
+TEBD using a 4th order Trotter decomposition.
+The time-evolution operator is approximated as ``U(τ₁)U(τ₂)U(τ₃)U(τ₂)U(τ₁)``
+with ``U(τᵢ) = exp(-iH_o τᵢ/2) exp(-i H_e τᵢ ) exp(-i H_o τᵢ/2)``,
+and ``τ1=τ2=1/(4-4^(1/3)) dt, τ3=dt - 4 τ1``.
+
+A reduction in the number of gate applications is achieved by grouping together
+the ``exp(-iH_o τᵢ/2)`` terms from consecutive Us and time-steps (except at measurement times).
+"""
+struct TEBD4 <: TEBDalg
 end
 
 function time_evo_gates(dt, H::GateList, alg::TEBD2)
@@ -15,9 +59,29 @@ function time_evo_gates(dt, H::GateList, alg::TEBD2)
     return [Uhalf ], Us, [Us[1], Uhalf]
 end
 
-function time_evo_gates(dt,H::GateList,alg::TEBD1)
+function time_evo_gates(dt,H::GateList,alg::TEBD2Sweep)
     Us = [exp.(-1im*dt/2 .* H), exp.(-1im*dt/2 .* H[end:-1:1])]
     return [], Us, []
+end
+
+function time_evo_gates(dt,H::GateList,alg::TEBD4)
+    τ₁ = 1/(4-4^(1/3))*dt
+    τ₂ = τ₁
+    τ₃ = dt - 2*τ₁ - 2*τ₂
+
+    e= 2:2:length(H)
+    o = 1:2:length(H)
+    Ustart = [exp.(-1im*τ₁/2 .* H[o])]
+
+    sequence = [(τ₁, e), (τ₁, o ), (τ₂,e), ((τ₂+τ₃)/2, o), (τ₃, e),
+                ((τ₂+τ₃)/2, o), (τ₂,e), (τ₂,o), (τ₁,e), (τ₁,o)]
+    Us = map(x->exp.(-1im*x[1] .* H[x[2]]), sequence)
+
+    end_sequence = [(τ₁, e), (τ₁, o ), (τ₂,e), ((τ₂+τ₃)/2, o), (τ₃, e),
+                    ((τ₂+τ₃)/2, o), (τ₂,e), (τ₂,o), (τ₁,e), (τ₁/2,o)]
+    Uend = map(x->exp.(-1im*x[1] .* H[x[2]]), end_sequence)
+
+    return Ustart, Us , Uend
 end
 
 tebd!(psi::MPS,H::BondOperator, args...; kwargs...) = tebd!(psi,gates(H),args...;kwargs...)
@@ -35,6 +99,8 @@ function tebd!(psi::MPS, H::GateList, dt::Number, tf::Number, alg::TEBDalg = TEB
     orthogonalize_step > 0 && (nbunch = gcd(nbunch,orthogonalize_step))
 
     Ustart, Us, Uend = time_evo_gates(dt,H,alg)
+
+    length(Ustart)==0 && length(Uend)==0 && (nbunch+=1)
 
     step = 0
     switchdir(dir) =dir== "fromleft" ? "fromright" : "fromleft"
@@ -60,7 +126,8 @@ function tebd!(psi::MPS, H::GateList, dt::Number, tf::Number, alg::TEBDalg = TEB
             apply_gates!(psi,U; dir=dir, kwargs...)
             dir = switchdir(dir)
         end
-        step += 1
+
+        length(Uend)>0 && (step += 1)
         (orthogonalize_step>0 && step % orthogonalize_step ==0) && reorthogonalize!(psi)
         observe!(obs,psi)
         checkdone!(obs,psi) && break
--- test/test_tebd.jl
+++ test/test_tebd.jl
@@ -2,33 +2,37 @@ using Test, TimeEvoMPS, ITensors
 using TimeEvoMPS: isleftortho, isrightortho
 
 @testset "Basic TEBD tests" begin
-    # check that evolving with identity doesn't change the state
-    N=10
-    sites = spinHalfSites(N)
-    trivialH = BondOperator(sites)
-    psi0 = randomMPS(sites)
-    orthogonalize!(psi0,1)
-    psi = deepcopy(psi0)
-    tebd!(psi,trivialH,0.01,1.;)
+    for alg in [TEBD2(), TEBD2Sweep(), TEBD4()]
+        @testset "$alg" begin
+            # check that evolving with identity doesn't change the state
+            N=10
+            sites = spinHalfSites(N)
+            trivialH = BondOperator(sites)
+            psi0 = randomMPS(sites)
+            orthogonalize!(psi0,1)
+            psi = deepcopy(psi0)
+            tebd!(psi,trivialH,0.01,1., alg)
 
-    @test inner(psi0,psi) ≈ 1
+            @test inner(psi0,psi) ≈ 1
 
-    #check that evolving forward and backward in time brings us back to the same state
-    #(up to numerical errors of course)
-    J,h = 0.3, -0.7
-    H = tfi_bondop(sites,J,h)
+            #check that evolving forward and backward in time brings us back to the same state
+            #(up to numerical errors of course)
+            J,h = 0.3, -0.7
+            H = tfi_bondop(sites,J,h)
 
-    psi0 = randomMPS(sites)
-    psi = deepcopy(psi0)
-    tebd!(psi,H,0.01,1.)
-    tebd!(psi,H,-0.01,-1.)
+            psi0 = randomMPS(sites)
+            psi = deepcopy(psi0)
+            tebd!(psi,H,0.01,1.,alg)
+            tebd!(psi,H,-0.01,-1.,alg)
 
-    @test inner(psi0,psi) ≈ 1
+            @test inner(psi0,psi) ≈ 1
 
-    #check that bond dimension is growing to maximum during evolution
-    psi = productMPS(sites,ones(Int,N))
-    tebd!(psi,H,0.01,5.)
-    @test maxLinkDim(psi) == 2^5
+            #check that bond dimension is growing to maximum during evolution
+            psi = productMPS(sites,ones(Int,N))
+            tebd!(psi,H,0.01,5.,alg)
+            @test maxLinkDim(psi) == 2^5
+        end
+    end
 end
 
 @testset "Imaginary time-evolution TFI model" begin
@@ -36,19 +40,22 @@ end
     J = 1.
     h = 0.5
     sites = spinHalfSites(N)
-    psi = productMPS(sites,ones(Int,N))
-    H = tfi_bondop(sites,J,h)
-    Hgates = gates(H)
+    for alg in [TEBD2(), TEBD2Sweep(), TEBD4()]
+        @testset "$alg" begin
+            psi = productMPS(sites,ones(Int,N))
+            H = tfi_bondop(sites,J,h)
+            Hgates = gates(H)
 
-    Es = []
-    for dt in [0.1,0.01,1e-3,1e-4]
-        tebd!(psi,H,-1im*dt,-1im*500*dt ; maxdim=50,orthogonalize=10)
-        push!(Es, measure(gates(H),psi))
+            Es = []
+            for dt in [0.1,0.01]
+                tebd!(psi,H,-1im*dt,-1im*500*dt, alg ; maxdim=50, cutoff=1e-8, orthogonalize=10)
+                push!(Es, measure(gates(H),psi))
+            end
+            # exact expression for ground-state energy
+            # at criticality (ref? took it from ITensors.jl tests)
+
+            eexact = 0.25 -0.25/sin(π/(4*N + 2))
+            @test Es[end] ≈ eexact atol=1e-4
+        end
     end
-
-    # exact expression for ground-state energy
-    # at criticality (ref? took it from ITensors.jl tests)
-
-    eexact = 0.25 -0.25/sin(π/(4*N + 2))
-    @test Es[end] ≈ eexact atol=1e-4
 end

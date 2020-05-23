ee442addbdb77a9b4671b0d3b7bfd934d6c595ff
orialb <orialb1@gmail.com>
2020-05-23 16:39:03 +0200

    fix tests (increase bond dim)

--- test/test_callbacks.jl
+++ test/test_callbacks.jl
@@ -1,5 +1,5 @@
 using Test, ITensors, TimeEvoMPS
-const te = TimeEvoMPS
+# const te = TimeEvoMPS
 
 @testset "NoTEvoCallback" begin
     obs = NoTEvoCallback()
@@ -19,26 +19,43 @@ end
     @test length(te.measurement_ts(cb))==0
 
     # make sure that no measurements are performed at wrong times
-    for (t,b,se) in [(0.03,1,true), (0.1,5,true), (0.1,1,false)]
-        te.apply!(cb, psi, t=t, bond = b, sweepend=se)
+    for (t,b,se) in [(0.03,1,true),(0.1,1,false)]
+        te.apply!(cb, psi, t=t, bond = b, sweepend=se,sweepdir="right", alg=TEBD2())
         @test length(te.measurement_ts(cb))==0
     end
 
-    te.apply!(cb, psi, t=0.1, bond = 1, sweepend=true)
+    orthogonalize!(psi,3)
+    te.apply!(cb, psi, t=0.1, bond = 3, sweepend=true, sweepdir="left",alg=TEBD2())
     @test length(te.measurement_ts(cb))==1
-    @test psi.llim==0
-    @test psi.rlim==2
     for o in ["Sz","Sx"]
-        @test length(te.measurements(cb)[o][end])==length(psi)
+        @test length(filter(x-> x!=0, te.measurements(cb)[o][end]))==2
+    end
+
+    cb = te.LocalMeasurementCallback(["Sz","Sx"], sites,0.1)
+    orthogonalize!(psi,1)
+    te.apply!(cb, psi, t=0.1, bond = 1, sweepend=true, sweepdir="left",alg=te.TDVP2())
+    @test length(te.measurement_ts(cb))==1
+    for o in ["Sz","Sx"]
+        @test length(filter(x->x!=0, te.measurements(cb)[o][end]))==2
+    end
+
+    cb = te.LocalMeasurementCallback(["Sz","Sx"], sites,0.1)
+    orthogonalize!(psi,3)
+    te.apply!(cb, psi, t=0.1, bond = 3, sweepend=true, sweepdir="left",alg=te.TDVP2())
+    @test length(te.measurement_ts(cb))==1
+    for o in ["Sz","Sx"]
+        @test length(filter(x->x!=0, te.measurements(cb)[o][end]))==1
     end
 
     N=10
+    sites = siteinds("S=1/2",N)
     H = tfi_bondop(sites,1.0,1.0)
     psi = productMPS(sites, fill("↑",N))
     cb = te.LocalMeasurementCallback(["Sz","Sx"], sites,0.5)
-    tebd!(psi,H,0.1,5,TEBD2(),maxdim=5,callback=cb)
+    tebd!(psi,H,0.1,5,TEBD2(),maxdim=30,callback=cb,verbose=true)
 
     @test length(te.measurement_ts(cb))==length(0.5:0.5:5)
+
     for i in 1:length(psi)
         orthogonalize!(psi,i)
         m = scalar(dag(psi[i])*noprime(op(sites, "Sz", i)*psi[i]))
@@ -48,15 +65,12 @@ end
     H = tfi_mpo(1.0,1.0,sites)
     psi = productMPS(sites, fill("↑",N))
     cb2 = te.LocalMeasurementCallback(["Sz","Sx"], sites,0.5)
-    tdvp!(psi,H,0.1,5,maxdim=5,hermitian=true,callback=cb2)
+    tdvp!(psi,H,0.1,5,maxdim=30,hermitian=true,callback=cb2)
 
     @test length(te.measurement_ts(cb2))==length(0.5:0.5:5)
     for i in 1:length(psi)
         orthogonalize!(psi,i)
-        m = scalar(dag(psi[i])*noprime(op(sites, "Sz", i)*psi[i]))
+        m = dot(psi[i], noprime(op(sites, "Sz", i)*psi[i]) )
         @test te.measurements(cb2)["Sz"][end][i] ≈ m
     end
-
 end
-
-

0166f420ecd50b09a2959b1903936ade4608f8c8
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2022-05-16 15:17:59 +0200

    gpu feature

--- src/tdvp.jl
+++ src/tdvp.jl
@@ -38,7 +38,70 @@ function tdvp!(psi,H::MPO,dt,tf; kwargs...)
     nsteps = Int(tf/dt)
     cb = get(kwargs,:callback, NoTEvoCallback())
     hermitian = get(kwargs,:hermitian,true)
-    exp_tol = get(kwargs,:exp_tol, 1e-14)
+    exp_tol = get(kwargs,:exp_tol, 1e-14) #tolerance
+    krylovdim = get(kwargs,:krylovdim, 30 )
+    maxiter = get(kwargs,:maxiter,100)
+    normalize = get(kwargs,:normalize,true)
+
+    pbar = get(kwargs,:progress, true) ? Progress(nsteps, desc="Evolving state... ") : nothing
+    τ = 1im*dt
+    imag(τ) == 0 && (τ = real(τ))
+
+    N = length(psi)
+    orthogonalize!(psi,1)
+    PH = ProjMPO(H)
+    position!(PH,psi,1)
+    for s in 1:nsteps
+        stime = @elapsed begin
+        for (b,ha) in sweepnext(N)
+            #evolve with two-site Hamiltonian
+            twosite!(PH)
+            ITensors.position!(PH,psi,b)
+            wf = psi[b]*psi[b+1]
+            wf, info = exponentiate(PH, -τ/2, wf; ishermitian=hermitian , tol=exp_tol, krylovdim=krylovdim)
+            dir = ha==1 ? "left" : "right"
+            info.converged==0 && throw("exponentiate did not converge")
+            spec = replacebond!(psi,b,wf;normalize=normalize, ortho = dir, kwargs... )
+            # normalize && ( psi[dir=="left" ? b+1 : b] /= sqrt(sum(eigs(spec))) )
+
+            apply!(cb,psi; t=s*dt,
+                   bond=b,
+                   sweepend= ha==2,
+                   sweepdir= ha==1 ? "right" : "left",
+                   spec=spec,
+                   alg=TDVP2())
+
+            # evolve with single-site Hamiltonian backward in time.
+            # In the case of imaginary time-evolution this step
+            # is not necessary (see Ref. [1])
+            i = ha==1 ? b+1 : b
+            if 1<i<N && !(dt isa Complex)
+                singlesite!(PH)
+                ITensors.position!(PH,psi,i)
+                psi[i], info = exponentiate(PH,τ/2,psi[i]; ishermitian=hermitian, tol=exp_tol, krylovdim=krylovdim,
+                                            maxiter=maxiter)
+                info.converged==0 && throw("exponentiate did not converge")
+            elseif i==1 && dt isa Complex
+                # TODO not sure if this is necessary anymore
+                psi[i] /= sqrt(real(scalar(dag(psi[i])*psi[i])))
+            end
+
+        end
+        end
+        !isnothing(pbar) && ProgressMeter.next!(pbar, showvalues=[("t", dt*s),
+                                                                  ("dt step time", round(stime,digits=3)),
+                                                                  ("Max bond-dim", maxlinkdim(psi))])
+        checkdone!(cb) && break
+    end
+end
+
+
+
+function tdvp!(psi,H::MPO,dt,tf; kwargs...)
+    nsteps = Int(tf/dt)
+    cb = get(kwargs,:callback, NoTEvoCallback())
+    hermitian = get(kwargs,:hermitian,true)
+    exp_tol = get(kwargs,:exp_tol, 1e-14) #tolerance
     krylovdim = get(kwargs,:krylovdim, 30 )
     maxiter = get(kwargs,:maxiter,100)
     normalize = get(kwargs,:normalize,true)
